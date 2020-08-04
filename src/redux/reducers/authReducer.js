dd2fd10f0fcdcf953cb9b169c641814e926bcab9
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-04 06:26:53 -0300

    redux auth reducer

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,112 @@
+import {
+    AUTH_USER_SIGNUP_PENDING,
+    AUTH_USER_SIGNUP_ERROR,
+    AUTH_USER_SIGNUP_SUCCESS,
+    AUTH_USER_SIGNUP_CLEAR,
+    AUTH_USER_SIGNIN_PENDING,
+    AUTH_USER_SIGNIN_ERROR,
+    AUTH_USER_SIGNIN_SUCCESS,
+    AUTH_USER_SIGNIN_CLEAR
+
+} from '../actions/types'
+
+const authState = {
+    isAuth: false,
+    signIn: {
+        isLoading: false,
+        error: false,
+        message: null,
+    },
+    signUp: {
+        isLoading: false,
+        error: false,
+        message: null,
+    },
+    user: {},
+    token: '',
+}
+
+function authReducer(state = authState, action) {
+    switch (action.type) {
+        case AUTH_USER_SIGNUP_PENDING:
+            return {
+                ...state,
+                signUp: {
+                    isLoading: true,
+                    error: null,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNUP_ERROR:
+            return {
+                ...state,
+                signUp: {
+                    isLoading: false,
+                    error: true,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNUP_SUCCESS:
+            return {
+                ...state,
+                signUp: {
+                    isLoading: false,
+                    error: false,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNUP_CLEAR:
+            return {
+                ...state,
+                signUp: {
+                    isLoading: false,
+                    error: false,
+                    message: null
+                }
+            }
+
+        case AUTH_USER_SIGNIN_PENDING:
+            return {
+                ...state,
+                signIn: {
+                    isLoading: true,
+                    error: null,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNIN_ERROR:
+            return {
+                ...state,
+                signIn: {
+                    isLoading: false,
+                    error: true,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNIN_SUCCESS:
+            return {
+                ...state,
+                isAuth: true,
+                user: action.payload.data.user,
+                token: action.payload.data.access_token,
+                signIn: {
+                    isLoading: false,
+                    error: false,
+                    message: action.payload.message
+                }
+            }
+        case AUTH_USER_SIGNIN_CLEAR:
+            return {
+                ...state,
+                signIn: {
+                    isLoading: false,
+                    error: false,
+                    message: null
+                }
+            }
+        default:
+            return state
+    }
+}
+
+export default authReducer;
