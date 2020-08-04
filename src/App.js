863fa8a1bd7ef8ae2d9a3c2c7bb1bfa6e0a29d7a
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-04 06:29:19 -0300

    redux store provider to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Layout from './components/Layout'
-import {BrowserRouter, Switch} from 'react-router-dom'
+import { BrowserRouter, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
         <BrowserRouter>
           <Layout>
             <Switch>
-              <Route exact path="/" render={(routeProps) => { return <div>Example route</div>}} />
+              <Route exact path="/" render={(routeProps) => { return <div>Example route</div> }} />
             </Switch>
           </Layout>
         </BrowserRouter>
+      </Provider>
     );
   }
 }
