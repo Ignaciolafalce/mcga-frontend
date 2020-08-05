3240827fe17957ce5368d908d196c4013b6a52c1
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-05 11:57:01 -0300

    Layout with mainnav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
-import React from 'React';
+import React from 'react';
+import MainNav from './MainNav';
 
 function Layout(props){
+    console.log(props);
     return(
-        <div class="container">
+        <div className="">
+            <MainNav></MainNav>
             {props.children}
+            <div className="fixed-bottom text-center bg-dark p-2" style={{color:'white'}}>MCGA - UAI 2020</div>
         </div>
     );
 }
