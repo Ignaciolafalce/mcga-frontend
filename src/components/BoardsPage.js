2b9c9d39c840f5e53b8270e5dc199ac9b07b2d56
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-06 01:12:20 -0300

    BoardPages Add, delete and edit actions

--- src/components/BoardsPage.js
+++ src/components/BoardsPage.js
@@ -1,11 +1,13 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useEffect, Fragment, useState } from 'react'
 import { connect } from 'react-redux'
 import PrivateRoute from './PrivateRoute';
 import { formatDateToString } from '../utils/helpers/index';
-import { getAllBoards,deleteBoard } from '../redux/actions/boardsActions';
+import { getAllBoards, deleteBoard } from '../redux/actions/boardsActions';
 import Loader from './Loader';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import AddBoardModal from './AddBoardModal';
+import DeleteBoardModal from './DeleteBoardModal';
 
 
 const mapStateToProps = (state) => {
@@ -21,79 +23,122 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         getAllBoards: () => { dispatch(getAllBoards()) },
-        deleteBoard: (boardId) => { dispatch(deleteBoard(boardId))}
     }
 }
 
 function BoardsPage(props) {
 
     useEffect(() => {
-        props.getAllBoards();
+        if (props.isAuth) {
+            props.getAllBoards();
+        }
     }, []);
 
+
+    const [showAddBoardModal, setShowAddBoardModal] = useState(false);
+    const toggleAddBoardModal = () => {
+        setShowAddBoardModal(!showAddBoardModal);
+    }
+
+    const [selectedBoard, setSelectedBoard] = useState({});
+    const [showEditBoardModal, setShowEditBoardModal] = useState(false);
+    const toggleEditBoardModal = () => {
+        if (showEditBoardModal) {
+            setSelectedBoard({});
+            setShowEditBoardModal(false);
+        }
+        if (!showEditBoardModal) {
+            setShowEditBoardModal(true);
+        }
+    }
+
+    const [showDeleteBoardModal, setShowDeleteBoardModal] = useState(false);
+    const toggleDeleteBoardModal = () => {
+        if (showDeleteBoardModal) {
+            setSelectedBoard({});
+            setShowDeleteBoardModal(false);
+        }
+        if (!showDeleteBoardModal) {
+            setShowDeleteBoardModal(true);
+        }
+    }
+
     return (
-        // <PrivateRoute>
-        <div class="container">
-            <div className="row justify-content-center mb-2">
-                <div className="col">
-                    <h1>Your boards</h1>
+        <PrivateRoute>
+            <div className="container">
+                <div className="row justify-content-center mb-2">
+                    <div className="col">
+                        <h1>Your boards</h1>
+                    </div>
+                    <div className="col text-right">
+                        <Button variant="warning" className="mr-2 mt-2" onClick={toggleAddBoardModal}>+ Add a new board</Button>
+                    </div>
                 </div>
-                <div className="col text-right">
-                <Button variant="warning" className="mr-2 mt-2">+ Add a new board</Button>
-                </div>
-            </div>
 
-            <Loader isVisible={props.isLoading} />
+                <Loader isVisible={props.isLoading} />
 
-            <div className="row justify-content-center">
-                <div className="col-11">
-                    {/* <AddBoardForm></AddBoardForm> */}
+                <div className="row justify-content-center">
+                    <div className="col-11">
+                        {/* <AddBoardForm></AddBoardForm> */}
 
-                    {props.boards && props.boards.length === 0 &&
-                        <div className="text-center">You dont have any board!, why dont you create one?</div>}
+                        {props.boards && props.boards.length === 0 &&
+                            <div className="text-center">You dont have any board!, why dont you create one?</div>}
 
-                    {props.boards && props.boards.length > 0 &&
-                        <Fragment>
-
-                            {props.boards.map(board =>
-                                <div key={board._id} className="row mb-2">
-                                    <div className="col bg-light p-2 rounded">
-                                        <div class="row justify-content-start m-2 rounded text-light">
-                                            <div className="col-9 rounded p-2 text-dark font-weight-bold">{board.name}</div>
-                                            <div className="col-3 text-right text-dark p-2 font-weight-light">{formatDateToString(board.created_at)}</div>
-                                        </div>
-                                        <div class="row justify-content-between">
-                                            <div className="col-5 m-2">
-                                                <Link to={`/board/${board._id}`} variant="secondary" className="mr-2">Go to this board</Link>
+                        {/* LIST OF BOARDS */}
+                        {props.boards && props.boards.length > 0 &&
+                            <Fragment>
+                                {props.boards.map(board =>
+                                    <div key={board._id} className="row mb-2">
+                                        <div className="col bg-light p-2 rounded">
+                                            <div className="row justify-content-start m-2 rounded text-light">
+                                                <div className="col-8 rounded p-2 text-dark font-weight-bold">{board.name}</div>
+                                                <div className="col-4 text-right text-dark p-2 font-weight-light">{formatDateToString(board.created_at)}</div>
                                             </div>
-                                            <div className="col text-right m-2">
-                                                <Button variant="primary" className="mr-2 mb-2" onClick={() => {
-                                                    props.deleteBoard(board._id);
-                                                }}>Edit</Button>
-                                                <Button className="mb-2" variant="danger">Delete</Button>
+                                            <div className="row justify-content-between">
+                                                <div className="col-5 m-2">
+                                                    <Button variant="secondary" className="btn-delete-board">
+                                                        <Link to={`/board/${board._id}`} className="mr-2">Go to this board</Link>
+                                                    </Button>
+                                                </div>
+                                                <div className="col text-right m-2">
+                                                    <Button variant="primary" className="mr-2 mb-2" onClick={() => {
+                                                        setSelectedBoard(board);
+                                                        toggleEditBoardModal();
+                                                    }}>
+                                                        Edit
+                                                    </Button>
+
+                                                    <Button className="mb-2" variant="danger" onClick={() => {
+                                                        setSelectedBoard(board);
+                                                        toggleDeleteBoardModal();
+                                                    }}>
+                                                        Delete
+                                                </Button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )}
-                        </Fragment>
-                    }
-                    {/* 
-                    {props.boards && props.boards.length > 0 &&
-                        <div>
-                            {props.boards.map(board =>
-                                <div key={board._id} className="bg-light p-5">
-                                    <Link to={`/board/${board._id}`}>
-                                        {formatDateToString(board.created_at)} - {board.name}
-                                    </Link>
-                                </div>
-                            )}
-                        </div>
-                    } */}
+                                )}
+                            </Fragment>
+                        }
+                    </div>
                 </div>
+
+                {/* MODALS */}
+                {showAddBoardModal &&
+                    <AddBoardModal show={showAddBoardModal} handleClose={toggleAddBoardModal} />
+                }
+
+                {showEditBoardModal &&
+                    <AddBoardModal show={showEditBoardModal} selectedBoard={selectedBoard} handleClose={toggleEditBoardModal} />
+                }
+
+                {showDeleteBoardModal &&
+                    <DeleteBoardModal show={showDeleteBoardModal} selectedBoard={selectedBoard} handleClose={toggleDeleteBoardModal} />
+                }
+
             </div>
-        </div>
-        // </PrivateRoute>
+        </PrivateRoute>
     );
 }
 
