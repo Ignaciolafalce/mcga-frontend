30d23adb5b5d83ea1a3b56a8f6f76202675fc170
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-09 08:08:27 -0300

    Api url changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    API_URL:'http://localhost:3030',
+    // API_URL:'http://localhost:3030',
+    API_URL:'',
     DEFAULT_ERROR_FETCH_MESSAGE:'Something went wrong, try it later'
 }
