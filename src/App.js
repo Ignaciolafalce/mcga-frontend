e99f0f755e9f8746ade21c67865ef53bc0573402
Ignacio Lafalce <nacho_lafalce@hotmail.com>
2020-08-09 21:34:21 -0300

    App unnecesary comments deleted

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import NotFound404 from './components/NotFound404';
-import HomePage from './components/HomePage';
-import SignInPage from './components/SignInPage';
-import BoardsPage from './components/BoardsPage';
-import BoardNotesPage from './components/BoardNotesPage';
+import Layout from './components/shared/Layout';
+import NotFound404 from './components/shared/NotFound404';
+import HomePage from './components/pages/HomePage';
+import SignInPage from './components/pages/SignInPage';
+import BoardsPage from './components/pages/BoardsPage';
+import BoardNotesPage from './components/pages/BoardNotesPage';
+import AuthWrapper from './components/shared/AuthWrapper';
 
 class App extends React.Component {
-
-  // componentWillMount(){
-  //   const token = localStorage.getItem('noteboards_t');
-  //   if(token){
-  //     store.dispatch({type:AUTH_USER_SIGNIN_SUCCESS, payload:{user:{}, access_token:token}});
-  //   }
-  // }
 
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <Layout>
-            <Switch>
-              <Route exact path="/" render={(routeProps) => { routeProps.history.push('/home')}} />
-              <Route exact path="/home" component={HomePage} />
-              <Route exact path="/signin" component={SignInPage} />
-              <Route exact path="/board/:boardId" component={BoardNotesPage} />
-              <Route exact path="/boards" component={BoardsPage} />
-              <Route component={NotFound404} />
-            </Switch>
-          </Layout>
+          <AuthWrapper>
+            <Layout>
+              <Switch>
+                <Route exact path="/" render={(routeProps) => { return routeProps.history.push('/home') }} />
+                <Route exact path="/home" component={HomePage} />
+                <Route exact path="/signin" component={SignInPage} />
+                <Route exact path="/board/:boardId" component={BoardNotesPage} />
+                <Route exact path="/boards" component={BoardsPage} />
+                <Route component={NotFound404} />
+              </Switch>
+            </Layout>
+          </AuthWrapper>
         </BrowserRouter>
       </Provider>
     );
